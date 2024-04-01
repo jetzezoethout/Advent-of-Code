614dfed5f0c0fd185418242e0a8d19cce0dcb6d6
Jetze Zoethout <j.zoethout22@gmail.com>
2024-04-01 09:38:35 +0200

    [2023 17] Queue Dijkstra from the undiscovered side
    Apparently, this is faster, as long as you construct the initial queue with fromAscList, and not with fromList.

--- 2023/day17/City.hs
+++ 2023/day17/City.hs
@@ -4,7 +4,7 @@ import           Coordinate (Coordinate (..))
 import           Data.Char  (digitToInt)
 import           Data.Text  (Text)
 import           Grid       (Grid (..), parseGrid)
-import           Node       (Node (..))
+import           Node       (Node (..), Orientation (Horizontal, Vertical))
 
 type City = Grid Int
 
@@ -14,3 +14,11 @@ parseCity = parseGrid digitToInt
 isFinalNode :: Node -> City -> Bool
 isFinalNode Node {..} city =
   coordinate.row == city.height - 1 && coordinate.column == city.width - 1
+
+allNodesAsc :: City -> [Node]
+allNodesAsc city =
+  [ Node (Coordinate r c) orient
+  | r <- [0 .. city.height - 1]
+  , c <- [0 .. city.width - 1]
+  , orient <- [Horizontal, Vertical]
+  ]
