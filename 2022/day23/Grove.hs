b79d14aa9fc64b1540862018762bbeb06ca1b639
Jetze Zoethout <j.zoethout22@gmail.com>
2024-06-15 16:08:29 +0200

    [2022 23] Simplified and slightly faster

--- 2022/day23/Grove.hs
+++ 2022/day23/Grove.hs
@@ -1,15 +1,13 @@
 module Grove where
 
 import           Control.Monad (guard)
-import           Coordinate    (Coordinate (..))
+import           Coordinate    (Coordinate (..), addCoordinates)
 import           Data.List     (find)
-import           Data.Maybe    (fromMaybe)
 import           Data.Set      (Set)
 import qualified Data.Set      as S
 import           Data.Text     (Text)
 import qualified Data.Text     as T
-import           Direction     (Direction (..), allDirections, clockWise,
-                                counterClockWise, moveTowards)
+import           Direction     (Direction (..), moveTowards)
 import           TaggedRow     (TaggedRow (..), parseTaggedLines)
 
 type Grove = Set Coordinate
@@ -36,10 +34,13 @@ roundsNeeded :: Grove -> Int
 roundsNeeded = go [North, South, West, East] 0
   where
     go preferredDirs acc grove =
-      case spreadOrFinish preferredDirs grove of
-        Nothing -> acc + 1
-        Just newGrove ->
-          go (tail preferredDirs <> [head preferredDirs]) (acc + 1) newGrove
+      let newGrove = spread preferredDirs grove
+       in if grove == newGrove
+            then acc + 1
+            else go
+                   (tail preferredDirs <> [head preferredDirs])
+                   (acc + 1)
+                   newGrove
 
 spreadTimes :: Int -> Grove -> Grove
 spreadTimes = go [North, South, West, East]
@@ -51,41 +52,39 @@ spreadTimes = go [North, South, West, East]
         . spread preferredDirs
 
 spread :: [Direction] -> Grove -> Grove
-spread preferredDirs grove =
-  fromMaybe grove $ spreadOrFinish preferredDirs grove
-
-spreadOrFinish :: [Direction] -> Grove -> Maybe Grove
-spreadOrFinish preferredDirs grove =
-  case S.foldr' addElf (0, S.empty) grove of
-    (0, _)        -> Nothing
-    (_, newGrove) -> Just newGrove
+spread preferredDirs grove = S.foldr' addElf grove grove
   where
     elf `canMoveTo` dir =
       all (`S.notMember` grove) $ elf `coordinatesTowards` dir
     movingDir elf =
       guard (any (`S.member` grove) (neighbours elf))
         >> find (elf `canMoveTo`) preferredDirs
-    addElf :: Coordinate -> (Int, Grove) -> (Int, Grove)
-    addElf elf (movements, acc) =
+    addElf :: Coordinate -> Grove -> Grove
+    addElf elf acc =
       case movingDir elf of
-        Nothing -> (movements, S.insert elf acc)
+        Nothing -> acc
         Just dir ->
           let target = elf `moveTowards` dir
            in if target `S.member` acc
-                then ( movements - 1
-                     , S.insert elf
-                         $ S.insert (target `moveTowards` dir)
-                         $ S.delete target acc)
-                else (movements + 1, S.insert target acc)
+                then S.insert (target `moveTowards` dir) $ S.delete target acc
+                else S.insert target $ S.delete elf acc
 
 coordinatesTowards :: Coordinate -> Direction -> [Coordinate]
-coord `coordinatesTowards` dir =
-  map
-    ($ (coord `moveTowards` dir))
-    [id, (`moveTowards` clockWise dir), (`moveTowards` counterClockWise dir)]
+coord `coordinatesTowards` dir = map (addCoordinates coord) deltas
+  where
+    deltas =
+      case dir of
+        North -> [Coordinate (-1) col | col <- [-1, 0, 1]]
+        East  -> [Coordinate row 1 | row <- [-1, 0, 1]]
+        South -> [Coordinate 1 col | col <- [-1, 0, 1]]
+        West  -> [Coordinate row (-1) | row <- [-1, 0, 1]]
 
 neighbours :: Coordinate -> [Coordinate]
-neighbours coord = allDirections >>= forwardAndRight
+neighbours coord = map (addCoordinates coord) deltas
   where
-    forwardAndRight dir =
-      map ($ (coord `moveTowards` dir)) [id, (`moveTowards` clockWise dir)]
+    deltas =
+      [ Coordinate row col
+      | row <- [-1, 0, 1]
+      , col <- [-1, 0, 1]
+      , not (row == 0 && col == 0)
+      ]
